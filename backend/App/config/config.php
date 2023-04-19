5a1a05e87de0f9f999103b27df1be2fc2f94977b
Lucas <lucasmnribeiro@gmail.com>
2023-04-19 20:43:20 -0300

    serverless: variaveis de ambiente para conectar ao banco de dados

--- backend/App/config/config.php
+++ backend/App/config/config.php
@@ -4,20 +4,20 @@
  * Configuration for: Error reporting
  * Useful to show every little problem during development, but only show hard errors in production
  */
-// error_reporting(E_ERROR);
-// ini_set("display_errors", 1);
+error_reporting(E_ERROR);
+ini_set("display_errors", 1);
 
 /**
  * Configuration for: Database
  * This is the place where you define your database credentials, database type etc.
  */
 define('DB_TYPE', 'pgsql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'market');
-define('DB_USER', 'postgres');
+define('DB_HOST', getenv('AWS_POSTGRES_HOST'));
+define('DB_NAME', getenv('AWS_POSTGRES_DB_NAME'));
+define('DB_USER', getenv('AWS_POSTGRES_DB_USER'));
 define('DB_PORT', '5432');
-define('DB_PASS', 'admin');
+define('DB_PASS', getenv('AWS_POSTGRES_DB_PASSWORD'));
 
 
 
-define('JWT_KEY', '1290839iasopd90-182i4e9-');
+define('JWT_KEY', getenv('AWS_JWT_KEY'));
